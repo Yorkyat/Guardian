fef51c71151a11c9fda0dc5dffb83f080eec4c16
Yorkyat <s1155062584@cuhk.edu.hk>
2018-07-31 18:55:32 +0800

    Add main character basic action with joystick, camera follow

---
+++ Assets/Script/MainCharacter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Joystick joystick;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+        if (moveVector != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveVector);
+            transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+    }
+
+    public void Firing()
+    {
+        if (anim.GetBool("Run"))
+        {
+            anim.Play("Run02_Attack");
+        }
+        else
+        {
+            anim.Play("Attack");
+        }
+    }
+}
