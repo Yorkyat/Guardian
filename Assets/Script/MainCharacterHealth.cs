3a731655e0a5d625b9cc4df2c01dc38614b9aea5
Yorkyat <s1155062584@cuhk.edu.hk>
2018-08-15 00:46:13 +0800

    Add slime attack

---
+++ Assets/Script/MainCharacterHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCharacterHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    private Animator anim;
+    private MainCharacter mainCharacter;
+    private bool isDead;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        mainCharacter = GetComponent<MainCharacter>();
+
+        currentHealth = startingHealth;
+    }
+
+    public void Damage(int amount)
+    {
+        currentHealth -= amount;
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+    
+    void Death()
+    {
+        // Prevent this function is called again
+        isDead = true;
+
+        anim.SetTrigger("Dead");
+
+        mainCharacter.enabled = false;
+    }
+}
---
+++ Assets/Script/SlimeAttack.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeAttack : MonoBehaviour
+{
+    public float attackOffset = 1.0f;
+    public int attackDamge = 10;
+
+    private Animator anim;
+    private GameObject player;
+    private MainCharacterHealth mainCharacterHealth;
+    private bool playerInRange;
+    private float attackTimer;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        mainCharacterHealth = player.GetComponent<MainCharacterHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+
+        if(attackTimer >= attackOffset && playerInRange)
+        {
+            Attack();
+        }
+
+        if(mainCharacterHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Attack()
+    {
+        // reset timer
+        attackTimer = 0f;
+
+        anim.Play("slime_attack");
+
+        if(mainCharacterHealth.currentHealth > 0)
+        {
+            mainCharacterHealth.Damage(attackDamge);
+        }
+    }
+}
