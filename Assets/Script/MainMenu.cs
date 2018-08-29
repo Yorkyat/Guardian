5150d6e6897ce723272997e966bd4be09a98502b
Yorkyat <s1155062584@cuhk.edu.hk>
2018-08-29 19:11:04 +0800

    Add loading screen

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Image loadingBar;
+    public GameObject loadingImage;
+
+    private AsyncOperation async;
 
     public void NewGame()
     {
-        SceneManager.LoadScene(1);
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(1));
+    }
+
+    IEnumerator LoadLevelWithBar(int level)
+    {
+        async = SceneManager.LoadSceneAsync(level);
+        while (!async.isDone)
+        {
+            loadingBar.fillAmount = async.progress;
+            yield return null;
+        }
     }
 
     public void Quit()
