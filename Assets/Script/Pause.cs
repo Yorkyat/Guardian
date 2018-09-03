e4ed8b430918a8f4d824437bf7f234381135ef8f
Yorkyat <s1155062584@cuhk.edu.hk>
2018-09-04 06:30:42 +0800

    Add pause button
    when paused, the joystick and fire button is disabled

---
+++ Assets/Script/Pause.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject joystick;
+    public GameObject fireButton;
+
+    private bool paused;
+    private FixedJoystick joystickScript;
+    private Button fireButtonScript;
+
+    void Awake()
+    {
+        joystickScript = joystick.GetComponent<FixedJoystick>();
+        fireButtonScript = fireButton.GetComponent<Button>();
+    }
+
+    void Start()
+    {
+        paused = false;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void PauseGame(bool state)
+    {
+        paused = state;
+
+        if (paused)
+        {
+            Time.timeScale = 0;
+            joystickScript.enabled = false;
+            fireButtonScript.enabled = false;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            joystickScript.enabled = true;
+            fireButtonScript.enabled = true;
+        }
+    }
+}
