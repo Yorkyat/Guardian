c0871a763232b4f9e47d7786e4dfde12d25b8699
Yorkyat <s1155062584@cuhk.edu.hk>
2018-11-16 16:15:52 +0800

    Update winning panel
    Add result calculation

--- Assets/Script/GameStageManager.cs
+++ Assets/Script/GameStageManager.cs
@@ -6,13 +6,32 @@ using UnityEngine.UI;
 public class GameStageManager : MonoBehaviour
 {
     public GameObject gameOverPanel;
+    public GameObject winPanel;
+    public MainCharacterHealth mainCharacterHealth;
+    public NumberAnimation numberAnimation;
+
     public Text enemyCounterText;
     public Text levelText;
     public Text timerText;
 
+    public Text resultLevelText;
+    public Text resultMinuteText;
+    public Text resultSecondText;
+    public Text resultHPText;
+    public Text resultLevelScoreText;
+    public Text resultTotalScoreText;
+
     private int curNoOfEnemy;
     private float stageStartTime;
+    private float stageStopTime;
+    private bool win = false;
     private bool gameOver = false;
+
+    private int level;
+    private float stageLastedTime;
+    private float hpLeftPercent;
+    private int levelScore;
+    private int totalScore;
 
     private void Start()
     {
@@ -26,10 +45,11 @@ public class GameStageManager : MonoBehaviour
 
     private void Update()
     {
-        if (!gameOver)
+        if (!gameOver && !win)
         {
-            int minute = (int)((Time.time - stageStartTime) / 60f);
-            int second = (int)((Time.time - stageStartTime) % 60f);
+            stageStopTime = Time.time;
+            int minute = (int)((stageStopTime - stageStartTime) / 60);
+            int second = (int)((stageStopTime - stageStartTime) % 60);
             timerText.text = minute.ToString("00") + ":" + second.ToString("00");
         }
     }
@@ -46,12 +66,46 @@ public class GameStageManager : MonoBehaviour
 
     private void Win()
     {
+        win = true;
 
+        level = GameManager.manager.playerData.currentLevel;
+        stageLastedTime = stageStopTime - stageStartTime;
+        hpLeftPercent = (float)mainCharacterHealth.currentHealth / (float)mainCharacterHealth.startingHealth * 100;
+        levelScore = 1000 + 100 * level + (int)(100 * (1 + hpLeftPercent / 100)) - (int)(2 * stageLastedTime);
+        totalScore = GameManager.manager.playerData.currentScore + levelScore;
+
+        winPanel.SetActive(true);
+        StartCoroutine(ResultCalculate());
     }
 
     public void GameOver()
     {
         gameOver = true;
         gameOverPanel.SetActive(true);
+    }
+
+    IEnumerator ResultCalculate()
+    {
+        // Set Level
+        resultLevelText.text = level.ToString();
+
+        // Set Time
+        numberAnimation.SetTextObject(resultMinuteText);
+        yield return StartCoroutine(numberAnimation.SetNum((int)stageLastedTime / 60, "00"));
+
+        numberAnimation.SetTextObject(resultSecondText);
+        yield return StartCoroutine(numberAnimation.SetNum((int)stageLastedTime % 60, "00"));
+
+        // Set HP Left
+        numberAnimation.SetTextObject(resultHPText);
+        yield return StartCoroutine(numberAnimation.SetNum(hpLeftPercent, "0"));
+
+        // Set Level Score
+        numberAnimation.SetTextObject(resultLevelScoreText);
+        yield return StartCoroutine(numberAnimation.SetNum(levelScore, "0"));
+
+        // Set Total Score
+        numberAnimation.SetTextObject(resultTotalScoreText);
+        yield return StartCoroutine(numberAnimation.SetNum(totalScore, "0"));
     }
 }
---
+++ Assets/Script/NumberAnimation.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberAnimation : MonoBehaviour
+{
+
+    public Text numberText;
+    public float animationTime = 1.5f;
+
+    private float desNum;
+    private float initNum;
+    private float curNum;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(numberText != null)
+        {
+            if(numberText.text != "")
+            {
+                curNum = float.Parse(numberText.text);
+            }
+            else
+            {
+                curNum = 0f;
+            }
+        }
+    }
+
+    public void SetTextObject(Text obj)
+    {
+        numberText = obj;
+        if (numberText.text != "")
+        {
+            curNum = float.Parse(numberText.text);
+        }
+        else
+        {
+            curNum = 0f;
+        }
+    }
+
+    public IEnumerator SetNum(float val, string format)
+    {
+        initNum = curNum;
+        desNum = val;
+        yield return StartCoroutine(Animate(format));
+    }
+
+    public IEnumerator AddToNum(float val, string format)
+    {
+        initNum = curNum;
+        desNum += val;
+        StartCoroutine(Animate(format));
+        yield return null;
+    }
+
+    public IEnumerator Animate(string format)
+    {
+        numberText.text = initNum.ToString(format);
+        while (curNum != desNum)
+        {
+            if (initNum < desNum)
+            {
+                curNum += (animationTime * Time.deltaTime) * (desNum - initNum);
+                if (curNum >= desNum)
+                {
+                    curNum = desNum;
+                }
+            }
+            else
+            {
+                curNum -= (animationTime * Time.deltaTime) * (initNum - desNum);
+                if (curNum <= desNum)
+                {
+                    curNum = desNum;
+                }
+            }
+            yield return null;
+
+            numberText.text = curNum.ToString(format);
+        }
+    }
+}
